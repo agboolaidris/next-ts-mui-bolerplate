c06fbb0444763fe9fe328917e0448c079cb54b4a
Iris <agboolaisholaidreez>
2022-08-14 21:01:19 -0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Button } from '../ui/atoms/button';
 
 const Home: NextPage = () => {
   return (
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
         <Typography variant="h4" component="h1" gutterBottom>
           MUI v5 + Next.js with TypeScript example
         </Typography>
+        <Button>HHHHH</Button>
       </Box>
     </Container>
   );
---
+++ src/ui/atoms/button.tsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+import { ButtonUnstyled } from '@mui/base';
+
+export const Button = styled(ButtonUnstyled)`
+  background-color: red;
+`;
