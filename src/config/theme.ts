feb2a0b70d870162e7f8de251ec58f5d0afb96c1
Iris <agboolaisholaidreez>
2022-08-14 20:17:34 -0700

    update

---
+++ src/config/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+export default theme;
